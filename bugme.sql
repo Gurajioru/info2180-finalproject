92d74ee9c8fd2bdac90b2a73e2bcedeb8c248fa0
Jelani Smith <rathalosrocks@gmail.com>
2021-12-02 20:31:27 -0500

    Database update

--- bugme.sql
+++ bugme.sql
@@ -1,31 +1,101 @@
-CREATE DATABASE IF NOT EXISTS bugme;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2021 at 11:55 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
-USE bugme;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-    `id` INTEGER AUTO_INCREMENT,
-    `firstname` VARCHAR(40),
-    `lastname` VARCHAR(40),
-    `pwrd` VARCHAR(40),
-    `email` VARCHAR(40),
-    `date_joined` DATETIME default CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-);
 
-DROP TABLE IF EXISTS `issues`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bugme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issues`
+--
+
 CREATE TABLE `issues` (
-    `id` INTEGER AUTO_INCREMENT,
-    `title` VARCHAR(64),
-    `_description` TEXT,
-    `_priority` VARCHAR(64),
-    `_type` VARCHAR(64),
-    `_status` VARCHAR(64),
-    `assigned_to` INTEGER,
-    `created_by` INTEGER,
-    `created` DATETIME,
-    `updated` DATETIME,
-    PRIMARY KEY(id));
+  `id` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `type` varchar(256) NOT NULL,
+  `priority` varchar(256) NOT NULL,
+  `status` varchar(256) NOT NULL,
+  `assigned_to` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
-GRANT ALL PRIVILEGES ON bugme.* TO 'admin'@'localhost' IDENTIFIED BY 'bugmin';
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(256) NOT NULL,
+  `lastname` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `date_joined` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `email`, `date_joined`) VALUES
+(1, 'John', 'Doe', '$2y$10$K.PKkn3uBcVLYP6VYnnXLekg5val8uk1TlIYXc7wyZETey9wmGylS', 'admin@project2.com', '2021-11-30 17:18:06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `issues`
+--
+ALTER TABLE `issues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `issues`
+--
+ALTER TABLE `issues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
